3f2b264d184f22d483d75171795061fac4f5a3d3
TravetskaN <ntravetskaya@gmail.com>
2022-01-20 12:10:58 +0200

    первое готово

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+timerId = null;
+btnStart.addEventListener('click', handlerColor);
+
+function handlerColor(event) {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+}
+
+btnStop.addEventListener('click', handlerStop);
+
+function handlerStop(event) {
+  if (timerId) {
+    clearInterval(timerId);
+    btnStart.disabled = false;
+  }
+}
