1d64ad26cd67acbf7aa16230591a97b017f7e8c7
TravetskaN <ntravetskaya@gmail.com>
2022-01-21 10:39:14 +0200

    02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -20,13 +20,12 @@ const options = {
       btnStart.disabled = false;
     } else {
       Notify.failure('Please choose a date in the future');
-      // window.alert("Please choose a date in the future")
     }
   },
 };
 
 const fpickr = flatpickr('#datetime-picker', options);
-// console.log("🚀 ~ fpickr---", fpickr.element.value);
+
 const daysEl = document.querySelector('span[data-days]');
 const hoursEl = document.querySelector('span[data-hours]');
 const minutesEl = document.querySelector('span[data-minutes]');
@@ -49,22 +48,21 @@ function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-  // Remaining days
+
   const days = Math.floor(ms / day);
-  // Remaining hours
+
   const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  // Remaining minutes
+
   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
+
   const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+console.log(convertMs(2000));
+console.log(convertMs(140000));
+console.log(convertMs(24140000));
