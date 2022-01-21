bee32edd2578cfcd792f4c775c0b0e525fd63f12
TravetskaN <ntravetskaya@gmail.com>
2022-01-21 10:51:48 +0200

    03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -50,8 +50,10 @@ function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
     if (shouldResolve) {
+      // Fulfill
       resolve({ position, delay });
     } else {
+      // Reject
       reject({ position, delay });
     }
   });
